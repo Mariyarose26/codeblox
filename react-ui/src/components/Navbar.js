f329deaf404bdc1e2322de6872784b9bd64d66e5
Mariya Rose <mariel.jesm@gmail.com>
2022-01-24 22:18:38 +1100

    2 Servers

---
+++ react-ui/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Navbar() {
+  return (<div id="navbar">
+            <img src="logo.png" alt="codeblox" id ="logo"/>
+          </div>
+          )
+}
+
+export default Navbar;
